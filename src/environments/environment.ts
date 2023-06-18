17408cced7dc5ada6f150e5669b9c9d32d786110
[Francisco Omedes] <[fomedes@uoc.edu]>
2023-06-19 00:19:42 +0100

    update api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true,
-  apiBaseUrl: 'https://road-to-glory-db-fdce9392c43f.herokuapp.com/api',
+  production: false,
+  apiBaseUrl: 'https://road-to-glory-db-fdce9392c43f.herokuapp.com/api/',
 };
