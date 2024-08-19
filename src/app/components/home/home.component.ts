9586a28219f81f7a241c29574a66b5a0a1ec8cbd
[Francisco Omedes] <omedesf@gmail.com>
2024-08-20 00:18:01 +0200

    fixed home button bug

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,9 +3,6 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButton } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
-import { CommunityDTO } from '../../models/community.dto';
-import { AuthService } from '../../services/auth.service';
-import { CommunityService } from '../../services/community.service';
 
 @Component({
   selector: 'app-home',
@@ -15,27 +12,7 @@ import { CommunityService } from '../../services/community.service';
   imports: [CommonModule, RouterLink, MatButton],
 })
 export class HomeComponent implements OnInit {
-  userCommunities: any[] = [];
-  user_id = '';
+  constructor() {}
 
-  constructor(
-    private communityService: CommunityService,
-    private authService: AuthService
-  ) {}
-
-  ngOnInit() {
-    this.user_id = this.authService.getUserId()!;
-    this.loadUserCommunities();
-  }
-
-  loadUserCommunities() {
-    this.communityService
-      .getUserCommunities(this.user_id)
-      .subscribe((communities: CommunityDTO[]) => {
-        this.userCommunities = communities.map((community) => ({
-          id: community.id,
-          name: community.name,
-        }));
-      });
-  }
+  ngOnInit() {}
 }
