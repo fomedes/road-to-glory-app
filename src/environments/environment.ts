bfaf759c5e3152320777ef740d19149b1486c297
[Francisco Omedes] <omedesf@gmail.com>
2024-09-03 23:57:20 +0200

    admin tools $ news display update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://road2glorytest.loca.lt/api/',
-};
-
 // export const environment = {
 //   production: false,
-//   apiUrl: 'https://road-to-glory-api.onrender.com/api/',
+//   apiUrl: 'https://road2glorytest.loca.lt/api/',
 // };
+
+  export const environment = {
+    production: false,
+    apiUrl: 'https://road-to-glory-api.onrender.com/api/',
+  };
